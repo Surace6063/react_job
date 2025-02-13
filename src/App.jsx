c2dcb5a8009aafc9a02d1df0c27a0792963eb33c
Suresh Thapa <zorosenpai456@gmail.com>
2025-02-13 12:12:08 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import NavBar from "./components/NavBar"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import JobPage from "./pages/JobPage"
+import AddJobPage from "./pages/AddJobPage"
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/job" element={<JobPage />} />
+        <Route path="/addjob" element={<AddJobPage />} />
+      </Routes>
     </>
   )
 }
